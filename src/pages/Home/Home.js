8debd856f01aa947228bbb1461250750923d43a7
Andjelina <astojanovic1900@gmail.com>
2022-11-26 21:55:35 +0100

    filter function

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { DataContext } from '../../App';
 import Overview from '../../components/Overview/Overview';
 import PageWrapper from '../../components/PageWrapper/PageWrapper';
 
 function Home() {
   const { sportEvents } = useContext(DataContext);
+  const [filteredEvent, setFilteredEvent] = useState(sportEvents);
 
   const renderOverview = () => {
-    return sportEvents.map((event, index) => (
+    return filteredEvent.map((event, index) => (
       <Overview key={index} event={event} index={index} />
     ));
   };
 
+  const filterStatus = (e) => {
+    let filtered = sportEvents.filter((el) => {
+      return el.status === e.target.name || e.target.name === 'all';
+    });
+    setFilteredEvent(filtered);
+  };
+
   return (
     <PageWrapper>
-      <div className="py-5">
+      <div className="page-breadcrumb d-flex justify-content-between align-items-center">
         <h1>Overview</h1>
+        <div className="d-flex gap-2 align-items-center">
+          <span>Filter by status</span>
+          <button
+            className="btn btn-sm btn-warning"
+            name="all"
+            onClick={filterStatus}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-sm btn-warning"
+            name="played"
+            onClick={filterStatus}
+          >
+            Played
+          </button>
+          <button
+            className="btn btn-sm btn-warning"
+            name="scheduled"
+            onClick={filterStatus}
+          >
+            Scheduled
+          </button>
+        </div>
       </div>
       <div className="row">{renderOverview()}</div>
     </PageWrapper>
